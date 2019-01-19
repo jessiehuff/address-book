f24922a3b1f3c69e3757a015029951b62a7f4565
jessiehuff <huffjd1@gmail.com>
2019-01-19 17:43:38 -0500

    Got ajax call to work

--- home/index.js
+++ home/index.js
@@ -1,35 +1,49 @@
-if (jQuery) {
-  alert("jquery is loaded"); 
-} else {
-  alert("Not loaded"); 
-}
+// if (jQuery) {
+//   alert("jquery is loaded"); 
+// } else {
+//   alert("Not loaded"); 
+// }
 
-// $(document).ready(function() {
-//   attachProjectListeners()
-// })
+$(document).ready(function() {
+  attachProjectListeners()
+})
 
 function attachProjectListeners() {
-  $('a.app-directory-item').on('click', function(e){
+  console.log("First")
+  $('.app-directory-item').on('click', function(e){
     e.preventDefault()
-    loadPeople(this,id)
+    console.log(this)
+    console.log("Here")
+    loadPeople(this)
+    console.log("Now Here")
   })
 }
 
-function loadPeople(element,id){
+function loadPeople(element){
   $.ajax({
     method: 'GET', 
     dataType: 'json', 
-    url: '/home', 
-  }).success(function(people){
-    if (people.length === 0) {
-      let $header = $('.app-directory-item')
-      $header.html('No people')
-    } else {
-      let $header = $('.app-directory-item')
-      $header.html('People')
-      peopleforEach(people => {
-        let 
-      })
+    url: '/api/people', 
+    success: function(resp, status, xhr){
+    console.log(element) 
     }
-  })
+    // if (people.length === 0) {
+    //   let $header = $('.app-directory-item')
+    //   $header.html('No people')
+    // } else {
+    //   let $header = $('.app-directory-item')
+    //   $header.html('People')
+    //   peopleforEach(people => {
+    //     let 
+      // })
+  }) 
 }
+
+// function People(people) {
+//   this.id = people.id 
+//   this.name = people.name 
+//   this.title 
+//   this.phone 
+//   this.email
+//   this.education
+// }
